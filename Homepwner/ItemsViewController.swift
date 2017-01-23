967c780dc2d01ba773b8b4b326524eb8fb2e0a4e
Jimmy Pocock <jimmypocock@yahoo.com>
2017-01-22 21:21:28 -0600

    Can move and delete items

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -8,6 +8,7 @@
 // Bonus Challenges: Page 194
 // Bronze: Sections
 // Gold: Customizing the Table
+// TODO: Make last row non-editable
 
 import UIKit
 
@@ -15,13 +16,44 @@ class ItemsViewController: UITableViewController {
 
     var itemStore: ItemStore!
 
+    @IBAction func addNewItem(_ sender: UIButton) {
+        // Create a new item and add it to the store
+        let newItem = itemStore.createItem()
+
+        // Figure out where that item is in the array
+        if let index = itemStore.allItems.index(of: newItem) {
+            let indexPath = IndexPath(row: index, section: 0)
+
+            // Insert this new row into the table
+            tableView.insertRows(at: [indexPath], with: .automatic)
+        }
+    }
+
+    @IBAction func toggleEditingMode(_ sender: UIButton) {
+        // If you are currently in editing mode...
+        if isEditing {
+            // Change text of button to inform user of state
+            sender.setTitle("Edit", for: .normal)
+
+            // Turn off editing mode
+            setEditing(false, animated: true)
+        } else {
+            // Change text of button to inform user of state
+            sender.setTitle("Done", for: .normal)
+
+            // Enter editing mode
+            setEditing(true, animated: true)
+        }
+    }
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemStore.allItems.count + 1
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let itemCount = itemStore.allItems.count
 
-        if indexPath.row < itemStore.allItems.count {
+        if indexPath.row < itemCount {
             // Get a new or recycled cell.
             let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
 
@@ -36,10 +68,43 @@ class ItemsViewController: UITableViewController {
             return cell
         } else {
             let cell = UITableViewCell(style: .default, reuseIdentifier: "LastCell")
-            cell.textLabel?.text = "You have \(itemStore.allItems.count) items."
+            cell.textLabel?.text = "You have \(itemCount) items."
 
             return cell
         }
+    }
+
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        // If the table view is asking to commit a delete command...
+        if editingStyle == .delete {
+            let item = itemStore.allItems[indexPath.row]
+
+            let title = "Delete \(item.name)?"
+            let message = "Are you sure you want to delete this item?"
+
+            let ac = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            ac.addAction(cancelAction)
+
+            let deleteAction = UIAlertAction(title: "Delete", style: .destructive, handler: { (action) -> Void in
+
+                // Remove the item from the store
+                self.itemStore.removeItem(item)
+
+                // Also remove that row from the table view with an animation
+                self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            })
+            ac.addAction(deleteAction)
+
+            // Present the alert controller
+            present(ac, animated: true, completion: nil)
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        // Update the model
+        itemStore.moveItem(from: sourceIndexPath.row, to: destinationIndexPath.row)
     }
 
     override func viewDidLoad() {
