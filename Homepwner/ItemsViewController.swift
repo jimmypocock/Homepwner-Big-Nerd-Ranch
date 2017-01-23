ed8051681b6cac0bba17802edee56dee9d888502
Jimmy Pocock <jimmypocock@yahoo.com>
2017-01-22 18:44:23 -0600

    Items View Controller with Table view

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -6,4 +6,8 @@
 //  Copyright © 2017 Jimmy Pocock. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+}
--- ItemsViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ItemsViewController.swift
-//  Homepwner
-//
-//  Created by Jimmy Pocock on 1/22/17.
-//  Copyright © 2017 Jimmy Pocock. All rights reserved.
-//
-
-import Foundation
