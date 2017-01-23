5ebd1c5ca06027d8ed8ebc8b3cc75bc04c31e90a
Jimmy Pocock <jimmypocock@yahoo.com>
2017-01-22 19:55:17 -0600

    Create a last row that says the number of items in item store.

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -7,7 +7,6 @@
 //
 // Bonus Challenges: Page 194
 // Bronze: Sections
-// Silver: Constant Rows
 // Gold: Customizing the Table
 
 import UIKit
@@ -17,24 +16,30 @@ class ItemsViewController: UITableViewController {
     var itemStore: ItemStore!
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemStore.allItems.count
+        return itemStore.allItems.count + 1
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
-        // Get a new or recycled cell.
-        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        if indexPath.row < itemStore.allItems.count {
+            // Get a new or recycled cell.
+            let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
 
+            // Set the text on the cell with the description of the item
+            // that is at the nth index of items, where n = row this cell
+            // will appear in on the tableview
+            let item = itemStore.allItems[indexPath.row]
 
-        // Set the text on the cell with the description of the item
-        // that is at the nth index of items, where n = row this cell
-        // will appear in on the tableview
-        let item = itemStore.allItems[indexPath.row]
+            cell.textLabel?.text = item.name
+            cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+            
+            return cell
+        } else {
+            let cell = UITableViewCell(style: .default, reuseIdentifier: "LastCell")
+            cell.textLabel?.text = "You have \(itemStore.allItems.count) items."
 
-        cell.textLabel?.text = item.name
-        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
-
-        return cell
+            return cell
+        }
     }
 
     override func viewDidLoad() {
